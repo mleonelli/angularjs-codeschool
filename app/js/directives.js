2012bc2a050676106912734d376588ea4cf27083
Mauro Leonelli <mleonelli@gmail.com>
2014-07-26 10:52:32 +0200

    Product Title Directive added - Http Service

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,17 @@
 /* Directives */
 
 
-angular.module('gemStore.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('gemStore.directives', [])
+
+	.directive('appVersion', ['version', function(version) {
+		return function(scope, elm, attrs) {
+			elm.text(version);
+		};
+	}])
+	
+	.directive('productTitle', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'partials/product-title.html'
+		};
+	});
