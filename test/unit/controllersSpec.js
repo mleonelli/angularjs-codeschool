29ae94153d8d129eb9da4c47e649d41b35b2f96b
Mauro Leonelli <mleonelli@gmail.com>
2014-07-27 11:11:04 +0200

    Basic unit testing added

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,18 +3,50 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  beforeEach(module('gemStore.controllers'));
 
 
-  it('should ....', inject(function($controller) {
+  it('ReviewController should be defined', inject(function($controller) {
     //spec body
-    var myCtrl1 = $controller('MyCtrl1', { $scope: {} });
-    expect(myCtrl1).toBeDefined();
+    var reviewCtrl = $controller('ReviewController', { $scope: {} });
+    expect(reviewCtrl).toBeDefined();
   }));
-
-  it('should ....', inject(function($controller) {
+  
+  /*it('ReviewController should add a review on AddReview', inject(function($controller) {
+    var scope = {},
+        reviewCtrl = $controller('ReviewController', { $scope: scope });
+	var product = {};
+		product.reviews = {};
+	scope.review = {};
+	reviewCtrl.addReview(product);
+    expect(product.reviews.length).toBe(1);
+  }));*/
+  
+  it('GalleryController should be defined', inject(function($controller) {
     //spec body
-    var myCtrl2 = $controller('MyCtrl2', { $scope: {} });
-    expect(myCtrl2).toBeDefined();
+    var galleryCtrl = $controller('GalleryController', { $scope: {} });
+    expect(galleryCtrl).toBeDefined();
   }));
+  
+  /*it('GalleryController should set current', inject(function($controller) {
+    var scope = {};
+    var galleryCtrl = $controller('GalleryController', { $scope: scope });
+	
+	scope.current = 0;
+	galleryCtrl.setCurrent(1);
+    expect(scope.current).toBe(1);
+  }));*/
+  
+  it('TabController should be defined', inject(function($controller) {
+    //spec body
+    var tabCtrl = $controller('TabController', { $scope: {} });
+    expect(tabCtrl).toBeDefined();
+  }));
+  
+  it('StoreController should be defined', inject(function($controller) {
+    //spec body
+    var storeCtrl = $controller('StoreController', { $scope: {} });
+    expect(storeCtrl).toBeDefined();
+  }));
+  
 });
