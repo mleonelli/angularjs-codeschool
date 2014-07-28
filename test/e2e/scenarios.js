d65a4f92b88e0096d8ff0ce8919a53d8eb916d08
Mauro Leonelli <mleonelli@gmail.com>
2014-07-28 16:16:20 +0200

    Adding protractor test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,11 +2,30 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
-describe('my app', function() {
+describe('gemStore', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('ReviewController should add a review on AddReview', function() {
+	var gemList = element.all(by.repeater('review in product.reviews'));
+    var stars = element(by.model('reviewCtrl.review.stars'));
+	var body = element(by.model('reviewCtrl.review.body'));
+	var author = element(by.model('reviewCtrl.review.author'));
+	
+    expect(gemList.count()).toBe(3);
+
+    stars.sendKeys('5');
+	body.sendKeys('test');
+	author.sendKeys('test@test.com');
+	
+	//var elem = ptor.findElement(protractor.By.id('StartJobButton'));
+	//elem.click().then( //stuff ).
+	
+    //expect(phoneList.count()).toBe(4);
+
+  }));
+  
+  /*it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
     expect(browser.getLocationAbsUrl()).toMatch("/view1");
   });
 
@@ -23,20 +42,6 @@ describe('my app', function() {
         toMatch(/partial for view 1/);
     });
 
-  });
+  });*/
 
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
